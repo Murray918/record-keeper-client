73df28d6539f61b14e6357390968ae352ff94dd0
Andrew Murray <darbyguitars@gmail.com>
2018-02-20 15:19:55 -0600

    pulling material ui cause i do not need it

---
+++ src/style/styles.js
@@ -0,0 +1,14 @@
+const styles = {
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around'
+	},
+	gridList: {
+		width: 500,
+		height: 450,
+		overflowY: 'auto'
+	}
+};
+
+export default styles;
