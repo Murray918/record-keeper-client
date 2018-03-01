b632a8d47deb27ac675fdd6db62e88d23cc2b673
Andrew Murray <darbyguitars@gmail.com>
2018-03-01 09:51:41 -0600

    styling the landing page

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,8 +3,18 @@ import landing from './images/landing.jpg';
 
 export default () => {
 	return (
-		<div id="landing" className="center">
-			<img id="landing-image" src={landing} alt="records" />
+		<div id="landing landing-container">
+			<img
+				id="landing-image"
+				calssName="u-full-max"
+				src={landing}
+				alt="records"
+			/>
+			<div id="landing-text">
+				<h1>TRACK</h1>
+				<h1>YOUR</h1> <h1>COLLECTION.</h1>
+				<h1>FEED</h1> <h1>YOUR</h1> <h1>PASSION.</h1>
+			</div>
 		</div>
 	);
 };
