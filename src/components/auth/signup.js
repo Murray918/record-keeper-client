73086a8b6b251fca663f852591933cdf00d2dc93
Andrew Murray <darbyguitars@gmail.com>
2018-12-26 16:46:54 -0600

    going to fix this bug on a seperate branch

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import SignupForm from './signup_form'
-import * as actions from '../../actions'
-import { Redirect } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import SignupForm from "./signup_form";
+import * as actions from "../../actions";
+import { Redirect } from "react-router-dom";
 
 class Signup extends Component {
-
   componentWillUnmount() {
     if (this.props.errorMessage) {
-      this.props.authError(null)
+      this.props.authError(null);
     }
   }
 
-  handleSubmit({email, password, passwordConfirmation}) {
-    this.props.signupUser({email, password, passwordConfirmation})
+  handleSubmit({ email, password, passwordConfirmation }) {
+    this.props.signupUser({ email, password, passwordConfirmation });
   }
 
   getRedirectPath() {
-    const locationState = this.props.location.state
+    const locationState = this.props.location.state;
     if (locationState && locationState.from.pathname) {
-      return locationState.from.pathname
+      return locationState.from.pathname;
     } else {
-      return '/'
+      return "/";
     }
   }
 
   render() {
-    return (this.props.authenticated) ?
-      <Redirect to={{
-        pathname: this.getRedirectPath(), state: {
-          from: this.props.location
-        }
-      }}/>
-      :
+    return this.props.authenticated ? (
+      <Redirect
+        to={{
+          pathname: this.getRedirectPath(),
+          state: {
+            from: this.props.location
+          }
+        }}
+      />
+    ) : (
       <div>
-        <SignupForm onSubmit={this.handleSubmit.bind(this)} errorMessage={this.props.errorMessage}/>
+        <SignupForm
+          onSubmit={this.handleSubmit.bind(this)}
+          errorMessage={this.props.errorMessage}
+        />
       </div>
+    );
   }
 }
 
@@ -43,7 +49,10 @@ function mapStateToProps(state) {
   return {
     authenticated: state.auth.authenticated,
     errorMessage: state.auth.error
-  }
+  };
 }
 
-export default connect(mapStateToProps, actions)(Signup)
+export default connect(
+  mapStateToProps,
+  actions
+)(Signup);
--- src/components/success_message.js
+++ src/components/success_message.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+//something is going wrong here
+//TODO: figure out why we are getting an error here
 export default class SuccessMessage extends Component {
 	componentWillMount() {
 		console.log(this.props);
 	}
 	render() {
 		if (this.props.recordMessage.record) {
+			console.log(this.props.recordMessage);
 			return (
 				<div className="u-full-width">
 					<strong> SUCCESS: </strong>
